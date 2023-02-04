e13e8743c411014bc0ffd984df438c355667132f
mjkonkel <mitchell.konkel@gmail.com>
2023-02-04 15:46:19 -0600

    added login

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,4 @@
-const {  User } = require('../models');
+const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
@@ -6,21 +6,39 @@ const resolvers = {
     Query: {
 
         me: async (parant, args, context) => {
-            if(context.user){ 
-                return await User.findOne({_id:context.user._id})
+            if (context.user) {
+                return await User.findOne({ _id: context.user._id })
             }
             console.log('not logged in')
         }
-        
-     
 
-    }, 
+    },
+
     Mutation: {
+
         addUser: async (parant, args) => {
             const user = await User.create(args);
             const token = signToken(user);
-            return {token, user}
-        }
+            return { token, user }
+        },
+
+        login: async (parant, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError('No user found with this email address');
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+            const token = signToken(user);
+            return { token, user };
+
+        },
+
+        saveBook: async (parant, { BookInput }) => {
+            const newBook = await Book.create()
+        },
     }
 
 };
